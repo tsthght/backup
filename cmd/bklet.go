69218b85eafd17493737a542800302f2fc2bd847
tsthght <tsthght@163.com>
2020-06-01 20:44:14 +0800

    http demo

--- cmd/bklet.go
+++ cmd/bklet.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tsthght/backup/http"
 )
 
 func main() {
 	fmt.Printf("i am bklet")
+	http.SetBinglogEnable(
+		"http://xxxxxx:8000/api/v1/cluster/conf_cluster_binlog",
+		"product",
+		"inf_blade_multiidc",
+		"xxxx",
+		true,
+		)
 	/*
 	wg := sync.WaitGroup{}
 	wg.Add(1)
