60ea13c62ed5b38750b7bb008dc5d528941ff5b4
tsthght <tsthght@163.com>
2020-06-02 14:11:47 +0800

    refine

--- cmd/bklet.go
+++ cmd/bklet.go
@@ -9,14 +9,20 @@ import (
 )
 
 func main() {
+	//参数解析
 	arg := args.Arguments{}
 	args.InitArgs(&arg)
 	fmt.Printf("cfg = %s\n", *arg.Cfg)
+	//配置文件解析
 	var conf config.BkConfig
 	if _, err := toml.DecodeFile(*arg.Cfg, &conf); err != nil {
 		fmt.Printf("error\n")
 	}
-	fmt.Printf("## %v\n", conf)
+	fmt.Printf("%v\n", conf)
+	//参数转化
+
+
+	//启动任务
 	/*
 	err := http.SetBinglogEnable(
 		"http://xxxxxx:8000/api/v1/cluster/conf_cluster_binlog",
