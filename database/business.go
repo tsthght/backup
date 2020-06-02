f3f14c6fe1b24b2e267ee5d7f52c4f9918a5a612
tsthght <tsthght@163.com>
2020-06-02 21:23:12 +0800

    add disk status

--- database/business.go
+++ database/business.go
@@ -7,10 +7,10 @@ import (
 
 const (
 	register_sql = "insert into bk_machine_info (ip) values (?) on duplicate key update update_time = now()"
-	status_sql = "replace into bk_machine_info(ip, " +
+	status_sql = "insert into bk_machine_info( ip, " +
 		"cpu_physic_core_num, cpu_logic_core_num, cpu_percent," +
 		"mem_total, mem_used, mem_used_percent, " +
-		"disk_path, disk_total, disk_free, disk_used_percent) values (?,    ?, ?, ?,    ?, ?, ?   ?, ?, ?, ?)"
+		"disk_path, disk_total, disk_free, disk_used_percent) values (?,   ?, ?, ?,    ?, ?, ?   ?, ?, ?, ?)"
 )
 
 func RegisterToCmdb(db *sql.DB, ip string) (int64, error) {
