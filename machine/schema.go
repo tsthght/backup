4b3e7ed3313af14c67a67079383cbdc338e15972
tsthght <tsthght@163.com>
2020-06-04 14:09:37 +0800

    add task

--- machine/schema.go
+++ machine/schema.go
@@ -28,57 +28,84 @@ func StateMachineSchema(initState int, db *sql.DB, ip string, uuid int) {
 			fmt.Printf("state: todo\n")
 			//更新状态
 			initState = PrepareEnv
-			database.SetMachineStageByIp(db, ip, "prepare_env")
+			err := database.SetMachineStageByIp(db, ip, "prepare_env")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "prepare_env")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case PrepareEnv:
 			fmt.Printf("state: prepare_env\n")
 			//更新状态
 			initState = PreCheck
-			database.SetMachineStageByIp(db, ip, "pre_check")
+			err := database.SetMachineStageByIp(db, ip, "pre_check")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "pre_check")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case PreCheck:
 			fmt.Printf("state: pre_check\n")
 			//更新状态
 			initState = Dumping
-			database.SetMachineStageByIp(db, ip, "dumping")
+			err := database.SetMachineStageByIp(db, ip, "dumping")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "dumping")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case Dumping:
 			fmt.Printf("state: dumping\n")
 			//更新状态
 			initState = Loading
-			database.SetMachineStageByIp(db, ip, "loading")
+			err := database.SetMachineStageByIp(db, ip, "loading")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "loading")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case Loading:
 			fmt.Printf("state: loading\n")
 			//更新状态
 			initState = PosCheck
-			database.SetMachineStageByIp(db, ip, "pos_check")
+			err := database.SetMachineStageByIp(db, ip, "pos_check")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "pos_check")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case PosCheck:
 			fmt.Printf("state: pos_check\n")
 			//更新状态
 			initState = ResetEnv
-			database.SetMachineStageByIp(db, ip, "reset_env")
+			err := database.SetMachineStageByIp(db, ip, "reset_env")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "reset_env")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case ResetEnv:
 			fmt.Printf("state: reset_env\n")
 			//更新状态
 			initState = Done
-			database.SetMachineStageByIp(db, ip, "done")
+			err := database.SetMachineStageByIp(db, ip, "done")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "done")
+			}
 			time.Sleep(2 * time.Second)
 			//todo
 		case Done:
 			fmt.Printf("state: done\n")
 			time.Sleep(2 * time.Second)
 			//更新状态
-			database.SetMachineStageByIp(db, ip, "idle")
-			database.SetTaskStageByUUID(db, uuid, "success")
+			err := database.SetMachineStageByIp(db, ip, "idle")
+			if err != nil {
+				fmt.Printf("call SetMachineStageByIp(%s, %s) failed\n", ip, "idle")
+			}
+			err = database.SetTaskStageByUUID(db, uuid, "success")
+			if err != nil {
+				fmt.Printf("call SetTaskStageByUUID(%s, %s) failed\n", ip, "success")
+			}
 			//设置任务状态
 			db.Close()
 			break
