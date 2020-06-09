ce00da0d6c023ddc87f968dc258c1f9232d6f7ae
tsthght <tsthght@163.com>
2020-06-09 17:27:12 +0800

    modify plugin

--- machine/all.go
+++ machine/all.go
@@ -31,7 +31,7 @@ func StateMachineAll(cluster *database.MGRInfo, user database.UserInfo, cfg conf
 			fmt.Printf("current num: %d, default num: %d\n", num, cfg.Task.DefaultPump)
 			if num >= cfg.Task.DefaultPump {
 				//更新状态为 修改配置文件
-				err := SetTaskState(cluster, user, uuid, "todo", "rolling_sql", "")
+				err := SetTaskState(cluster, user, uuid, "todo", "open_binlog", "")
 				if err != nil {
 					fmt.Printf("call SetTaskState failed. err : %s", err.Error())
 					continue
@@ -49,6 +49,12 @@ func StateMachineAll(cluster *database.MGRInfo, user database.UserInfo, cfg conf
 			err = SetMachineStateByIp(cluster, user, ip, "pump")
 			if err != nil {
 				fmt.Printf("call SetMachineStateByIp failed. err : %s", err.Error())
+				continue
+			}
+			//设置 task的状态
+			err = SetTaskState(cluster, user, uuid, "todo", "pump", "")
+			if err != nil {
+				fmt.Printf("call SetTaskState failed. err : %s", err.Error())
 				continue
 			}
 			//启动pump
